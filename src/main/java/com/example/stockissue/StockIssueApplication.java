df1b29fc211d46fa86de3236ba3ad1ba51ef4b6b
alsdl0629 <ehfehf0629@gmail.com>
2023-12-13 20:09:25 +0900

    Init Project

---
+++ src/main/java/com/example/stockissue/StockIssueApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockIssueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockIssueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stockissue/StockIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockIssueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
