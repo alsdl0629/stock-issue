a97e0f485b54843989602c8f8e9f0cc4fe149a95
alsdl0629 <ehfehf0629@gmail.com>
2023-12-14 09:13:19 +0900

    Pessimistic Lock을 사용해 레이스 컨디션 해결

--- src/main/java/com/example/stockissue/repository/StockRepository.java
+++ src/main/java/com/example/stockissue/repository/StockRepository.java
@@ -1,7 +1,15 @@
 package com.example.stockissue.repository;
 
 import com.example.stockissue.domain.Stock;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    // Spring Data Jpa에서는 @Lock을 통해서 Pessimistic Lock을 구현할 수 있다.
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select s from Stock s where s.id = :id")
+    Stock findByIdWithPessimisticLock(Long id);
 }
